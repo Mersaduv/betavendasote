02c97a811f9752658d2638b793f6e911bca0e5d8
Mersaduv <mersadkhahan70@gmail.com>
2025-03-30 18:54:47 +0330

    update code

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -4,9 +4,7 @@ const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     // baseUrl: 'https://localhost:7004',
-    baseUrl: `${
-      typeof window !== 'undefined' && window.location.protocol === 'https:' ? 'https' : 'http'
-    }://45.159.150.230`,
+    baseUrl: 'https://45.159.150.230',
     // baseUrl: 'https://apivendamode.liara.run',
     timeout: 60000,
   }),
